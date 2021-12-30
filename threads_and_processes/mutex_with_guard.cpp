42d80d52b80716a1b0782d8ec7d07c9654ef9969
aponte411 <aponted511@gmail.com>
2021-12-30 13:24:54 -0500

    feat: adding mutex with guard example

---
+++ threads_and_processes/mutex_with_guard.cpp
@@ -0,0 +1,31 @@
+#include <map>
+#include <string>
+#include <chrono>
+#include <thread>
+#include <mutex>
+
+// Global key value store
+std::map<std::string, std::string> web_pages;
+std::mutex web_pages_mutex;
+
+void save_page(const std::string& url){
+    // Simulate long page fetch
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    std::string result = "Fake content from web page";
+    // lock
+    std::lock_guard<std::mutex> web_pages_guard(web_pages_mutex);
+    // Write results to key value store
+    web_pages[url] = result;
+}
+
+int main(){
+    std::thread thread1(save_page, "http://foo");
+    std::thread thread2(save_page, "http://bar");
+    thread1.join();
+    thread2.join();
+    // Print results
+    for (const auto& web_page: web_pages){
+        printf("Url: %s\n", web_page.first);
+        printf("Result: %s\n", web_page.second);
+    }
+}
